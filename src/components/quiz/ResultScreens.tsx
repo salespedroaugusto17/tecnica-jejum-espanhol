d44e2c5859b1713cb324be6fb6cac5f0b3984cb3
salespedroaugusto17 <salespedroaugusto17@gmail.com>
2026-07-24 17:05:00 -0300

    lp-preço-29

--- src/components/quiz/ResultScreens.tsx
+++ src/components/quiz/ResultScreens.tsx
@@ -214,8 +214,8 @@ function CountdownTimer() {
 }
 
 function OfferCard({ onCTA, gender }: { onCTA: () => void; gender?: string }) {
-  const parcelado = "7,29";
-  const aVista = "37,00";
+  const parcelado = "5,72";
+  const aVista = "29,00";
 
   return (
     <div className="flex flex-col gap-3">
